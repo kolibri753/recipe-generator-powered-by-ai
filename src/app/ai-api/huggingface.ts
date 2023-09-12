9a1aec6cfc64fb6584e3e67fe0dc859822490150
kolibri753 <kolibri753@gmail.com>
2023-09-12 21:06:31 +0300

    feat(ai): add Hugging Face GPT-2 model
    This commit adds the Hugging Face GPT-2 model to the project, enabling AI-powered text generation for recipe suggestions.

---
+++ src/app/ai-api/huggingface.ts
@@ -0,0 +1,18 @@
+import { HfInference } from "@huggingface/inference";
+
+const hf = new HfInference(process.env.HUGGINGFACE_API);
+
+export async function generateText(ingredients: string, continuation: string = "") {
+  try {
+    const response = await hf.textGeneration({
+      model: "gpt2",
+      inputs: `Generate a recipe using the following ingredients: ${ingredients}\n${continuation}`,
+      parameters: { max_new_tokens: 250 },
+    });
+
+    return response;
+  } catch (error) {
+    console.error("Error generating text:", error);
+    return null;
+  }
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,96 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import { generateText } from "./ai-api/huggingface";
 
 export default function Home() {
+	const [ingredients, setIngredients] = useState("");
+	const [generatedText, setGeneratedText] = useState("");
+	const [continuation, setContinuation] = useState("");
+	const [showContinueButton, setShowContinueButton] = useState(true);
+	const [isLoading, setIsLoading] = useState(false); // Add loading state
+
+	const handleSubmit = async (e: { preventDefault: () => void; }) => {
+		e.preventDefault();
+		setIsLoading(true); // Set loading to true when submitting
+		if (ingredients) {
+			// Log the input to the AI
+			console.log("Input to AI:", ingredients);
+
+			const response = await generateText(ingredients, continuation);
+			if (response && response.generated_text) {
+				// Extract only the generated portion (excluding initial input)
+				const generatedRecipe = response.generated_text.replace(
+					`Generate a recipe using the following ingredients: ${ingredients}`,
+					""
+				);
+				setGeneratedText(generatedText + generatedRecipe.trim()); // Append the new text
+				// Log the generated text
+				console.log("Generated Text:", generatedText + generatedRecipe.trim());
+				setShowContinueButton(true); // Show the "Continue" button after generating
+			}
+			setIsLoading(false); // Set loading to false after generating
+		} else {
+			setIsLoading(false); // Set loading to false if no ingredients are entered
+		}
+	};
+
+	const handleContinue = async () => {
+		if (generatedText) {
+			// Append the current generated text to the continuation
+			const newContinuation = `${continuation}\n${generatedText}`;
+			setContinuation(newContinuation);
+			// Hide the "Continue" button
+			setShowContinueButton(false);
+
+			// Log the input to the AI for the continuation
+			console.log("Input to AI (Continuation):", newContinuation);
+
+			// Generate a new response using ingredients + the first generated part
+			const response = await generateText(ingredients, newContinuation);
+			if (response && response.generated_text) {
+				// Extract only the new generated portion
+				const newGeneratedRecipe = response.generated_text.replace(
+					newContinuation,
+					""
+				);
+				setGeneratedText(generatedText + newGeneratedRecipe.trim()); // Append the new text
+				// Log the new generated text
+				console.log("Continue:", generatedText + newGeneratedRecipe.trim());
+				setShowContinueButton(true); // Show the "Continue" button again
+			}
+		}
+	};
+
 	return (
 		<main>
 			<h1>Recipe Generator Powered by AI</h1>
-			{/* Your content goes here */}
+			<form onSubmit={handleSubmit}>
+				<label>
+					Enter Ingredients:
+					<input
+						type="text"
+						value={ingredients}
+						onChange={(e) => setIngredients(e.target.value)}
+					/>
+				</label>
+				<button type="submit">Generate Recipe</button>
+			</form>
+			{isLoading ? (
+				<p>Loading...</p>
+			) : (
+				<>
+					{generatedText && (
+						<div>
+							<p>Generated Text: {generatedText}</p>
+							{showContinueButton && (
+								<button onClick={handleContinue}>Continue</button>
+							)}
+						</div>
+					)}
+				</>
+			)}
+			{/* Content goes here */}
 		</main>
 	);
 }
