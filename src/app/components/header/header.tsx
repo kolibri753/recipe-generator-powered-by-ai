37df231ad04c9c8a5b6ec6ffb84a2d43b8c77c42
kolibri753 <kolibri753@gmail.com>
2023-09-15 21:08:13 +0300

    feat(header): create header component
    Created a new header component that displays the FoodieMate AI logo and includes a theme changer button.

---
+++ src/app/components/header/header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "./header.module.css";
+import Image from "next/image";
+import ThemeChangerBtn from "../themeChangerBtn/themeChangerBtn";
+
+import logo from "/public/images/logo.png";
+
+const Header = () => {
+	return (
+		<header className={styles.header}>
+			<div className={styles.logo}>
+				<Image
+					src={logo}
+					width={80}
+					height={80}
+					alt="Logo"
+					className={styles.logo__icon}
+					style={{ objectFit: "contain" }}
+				/>
+				<span className={styles.logo__text}>FoodieMate AI</span>
+			</div>
+			<ThemeChangerBtn />
+		</header>
+	);
+};
+
+export default Header;
