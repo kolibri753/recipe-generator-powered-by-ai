b8a593fe285132dfb1ae57591ae587660340d07a
kolibri753 <kolibri753@gmail.com>
2023-09-21 09:22:03 +0300

    feat(component): create and style SearchBar component
    Created a new SearchBar component and applied styling using BEM methodology.

---
+++ src/app/components/searchBar/searchBar.tsx
@@ -0,0 +1,43 @@
+// SearchBar.tsx
+import React from "react";
+import styles from "./searchBar.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faReceipt } from "@fortawesome/free-solid-svg-icons";
+
+interface SearchBarProps {
+	ingredients: string;
+	onIngredientsChange: (value: string) => void;
+	onSearch: () => void;
+}
+
+function SearchBar({
+	ingredients,
+	onIngredientsChange,
+	onSearch,
+}: SearchBarProps) {
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onSearch();
+	};
+
+	return (
+		<form onSubmit={handleSubmit} className={styles.searchBar}>
+			<label className={styles.searchBar__label}>
+				Enter Ingredients:
+				<input
+					className={styles.searchBar__input}
+					type="text"
+					value={ingredients}
+          size={100}
+					onChange={(e) => onIngredientsChange(e.target.value)}
+				/>
+			</label>
+			<button type="submit" className={styles.searchBar__button}>
+				<span className={styles.button__text}>Generate Recipe</span>
+				<FontAwesomeIcon className={styles.button__icon} icon={faReceipt} />
+			</button>
+		</form>
+	);
+}
+
+export default SearchBar;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,82 +1,76 @@
-"use client";
+"use client"
 
 import React, { useState } from "react";
 import styles from "./page.module.css";
 import Header from "./components/header/header";
 import AvatarAppIntro from "./components/avatarAppIntro/avatarAppIntro";
 import Recipe from "./components/recipe/recipe";
+import SearchBar from "./components/searchBar/searchBar";
 
 import { generateText } from "./ai-api/huggingface";
 
 export default function Home() {
-	const [ingredients, setIngredients] = useState("");
-	const [initialRecipe, setInitialRecipe] = useState<string>("");
-	const [continueRecipe, setContinueRecipe] = useState<string>("");
-	const [isLoading, setIsLoading] = useState(false);
-	const [showInitialRecipe, setShowInitialRecipe] = useState(true);
+  const [ingredients, setIngredients] = useState("");
+  const [initialRecipe, setInitialRecipe] = useState<string>("");
+  const [continueRecipe, setContinueRecipe] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [showInitialRecipe, setShowInitialRecipe] = useState(true);
 
-	const handleSubmit = async (e: { preventDefault: () => void }) => {
-		e.preventDefault();
-		setIsLoading(true);
+  const handleSearch = async () => {
+    setIsLoading(true);
 
-		if (ingredients) {
-			console.log("Submitting ingredients:", ingredients);
-			const response = await generateText(ingredients);
+    if (ingredients) {
+      console.log("Submitting ingredients:", ingredients);
+      const response = await generateText(ingredients);
 
-			if (response && response.generated_text) {
-				setInitialRecipe(response.generated_text);
-				console.log("Generated Recipe Text:", response.generated_text);
-			}
+      if (response && response.generated_text) {
+        setInitialRecipe(response.generated_text);
+        console.log("Generated Recipe Text:", response.generated_text);
+      }
 
-			setIsLoading(false);
-		} else {
-			setIsLoading(false);
-		}
-	};
+      setIsLoading(false);
+    } else {
+      setIsLoading(false);
+    }
+  };
 
-	const handleContinue = async () => {
-		if (initialRecipe) {
-			console.log("Continuing with generated text:", initialRecipe);
-			const response = await generateText(ingredients, initialRecipe);
+  const handleContinue = async () => {
+    if (initialRecipe) {
+      console.log("Continuing with generated text:", initialRecipe);
+      const response = await generateText(ingredients, initialRecipe);
 
-			if (response && response.generated_text) {
-				setContinueRecipe(response.generated_text);
-				console.log("New Generated Recipe Text:", response.generated_text);
-				setShowInitialRecipe(false);
-			}
-		}
-	};
+      if (response && response.generated_text) {
+        setContinueRecipe(response.generated_text);
+        console.log("New Generated Recipe Text:", response.generated_text);
+        setShowInitialRecipe(false);
+      }
+    }
+  };
 
-	const handleReturn = () => {
-		if (!showInitialRecipe) {
-			setShowInitialRecipe(true);
-			console.log("return is pressed");
-		}
-	};
+  const handleReturn = () => {
+    if (!showInitialRecipe) {
+      setShowInitialRecipe(true);
+      console.log("Return is pressed");
+    }
+  };
 
-	return (
-		<main>
-			<Header />
-			<form onSubmit={handleSubmit}>
-				<label>
-					Enter Ingredients:
-					<input
-						type="text"
-						value={ingredients}
-						onChange={(e) => setIngredients(e.target.value)}
-					/>
-				</label>
-				<button type="submit">Generate Recipe</button>
-			</form>
-			<section className={styles.middle}>
-				<AvatarAppIntro />
-				<Recipe
-					recipeText={showInitialRecipe ? initialRecipe : continueRecipe}
-					isLoading={isLoading}
-					onContinue={handleContinue}
-					onReturn={handleReturn}
-				/>
-			</section>
-		</main>
-	);
+  return (
+    <main className={styles.main}>
+      <Header />
+      <SearchBar
+        ingredients={ingredients}
+        onIngredientsChange={setIngredients}
+        onSearch={handleSearch}
+      />
+      <section className={styles.middle}>
+        <AvatarAppIntro />
+        <Recipe
+          recipeText={showInitialRecipe ? initialRecipe : continueRecipe}
+          isLoading={isLoading}
+          onContinue={handleContinue}
+          onReturn={handleReturn}
+        />
+      </section>
+    </main>
+  );
 }
