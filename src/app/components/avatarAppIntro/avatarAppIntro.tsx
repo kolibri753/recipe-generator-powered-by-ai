8dd055a133b64157f27eb12b73903a096e9c3f4b
kolibri753 <kolibri753@gmail.com>
2024-10-07 10:11:08 +0300

    refactor: use library for typewriter-effect

--- src/app/components/avatarAppIntro/avatarAppIntro.tsx
+++ src/app/components/avatarAppIntro/avatarAppIntro.tsx
@@ -1,43 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./avatarAppIntro.module.css";
 import Image from "next/image";
-
+import Typewriter from "typewriter-effect";
 import avatar from "/public/images/avocado.png";
 
 const AvatarAppIntro = () => {
-	// State to control the typing animation
-	const [typedText, setTypedText] = useState("");
 	const introText =
 		"Welcome to FoodieMate AI! I'm your friendly kitchen companion here to help you explore a world of delightful recipes. Let's embark on a delicious journey together!";
 
-	useEffect(() => {
-		const typeText = () => {
-			const delay = 100;
-			let currentIndex = 0;
-
-			const typingInterval = setInterval(() => {
-				if (currentIndex < introText.length) {
-					setTypedText(introText.slice(0, currentIndex + 1));
-					currentIndex++;
-				} else {
-					clearInterval(typingInterval);
-				}
-			}, delay);
-		};
-
-		typeText();
-
-		return () => {
-			// Cleanup if needed
-		};
-	}, []);
-
 	return (
 		<div className={styles.avatar}>
-			<div
-				className={[styles.avatar__textContainer, styles.avatar__thinking].join("")}
-			>
-				<p className={styles.avatar__text}>{typedText}</p>
+			<div className={styles.avatar__textContainer}>
+				<Typewriter
+					options={{
+						strings: [introText],
+						autoStart: true,
+						loop: true,
+						delay: 75,
+						wrapperClassName: styles.avatar__text,
+					}}
+				/>
 			</div>
 			<div className={styles.avatar__imageContainer}>
 				<Image
