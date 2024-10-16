491ec5646a5ad049d61f6828aa94506c575bb5f7
kolibri753 <kolibri753@gmail.com>
2024-10-16 10:39:22 +0300

    refactor: call useTheme() to access the resolvedTheme

--- src/app/components/themeChangerBtn/themeChangerBtn.tsx
+++ src/app/components/themeChangerBtn/themeChangerBtn.tsx
@@ -1,16 +1,15 @@
-"use client"
+"use client";
 
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./themeChangerBtn.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCarrot, faCookie } from "@fortawesome/free-solid-svg-icons";
 import { useTheme } from "next-themes";
 
 const ThemeChangerBtn = () => {
+	const { setTheme, resolvedTheme } = useTheme();
 	const [mounted, setMounted] = useState(false);
-	const { theme, setTheme } = useTheme(); // Use the useTheme hook to access theme
 
-	// useEffect only runs on the client, so now we can safely show the UI
 	useEffect(() => {
 		setMounted(true);
 	}, []);
@@ -20,21 +19,21 @@ const ThemeChangerBtn = () => {
 	}
 
 	const toggleTheme = () => {
-		// Toggle between light and dark themes
-		setTheme(theme === "light" ? "dark" : "light");
+		const newTheme = resolvedTheme === "light" ? "dark" : "light";
+		setTheme(newTheme);
 	};
 
 	return (
 		<div
 			className={`${styles.theme__switcher} ${
-				theme === "dark" ? styles.darkMode : ""
+				resolvedTheme === "dark" ? styles.darkMode : ""
 			}`}
 			onClick={toggleTheme}
 		>
 			<div className={styles.theme__slider}>
 				<FontAwesomeIcon
 					className={styles.theme__icon}
-					icon={theme === "dark" ? faCookie : faCarrot}
+					icon={resolvedTheme === "dark" ? faCookie : faCarrot}
 				/>
 			</div>
 		</div>
