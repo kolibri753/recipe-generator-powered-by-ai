91005bdba9d925fb7022beb6d5b5e9749994138b
kolibri753 <kolibri753@gmail.com>
2024-10-17 11:24:56 +0300

    feat: remove continue-return buttons and their related logic

--- src/app/components/recipe/recipe.tsx
+++ src/app/components/recipe/recipe.tsx
@@ -1,10 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-	faArrowLeft,
-	faArrowRight,
-	faShare,
-} from "@fortawesome/free-solid-svg-icons";
+import { faShare } from "@fortawesome/free-solid-svg-icons";
 import Background from "./background";
 import { shareRecipe } from "../../utils/shareUtils";
 
@@ -13,28 +9,9 @@ import styles from "./recipe.module.css";
 interface RecipeProps {
 	recipeText: string;
 	isLoading: boolean;
-	onContinue: () => void;
-	onReturn: () => void;
 }
 
-const Recipe: React.FC<RecipeProps> = ({
-	recipeText,
-	isLoading,
-	onContinue,
-	onReturn,
-}) => {
-	const [isReturnDisabled, setIsReturnDisabled] = useState(true);
-
-	const handleReturnClick = () => {
-		onReturn();
-		setIsReturnDisabled(true);
-	};
-
-	const handleContinueClick = () => {
-		onContinue();
-		setIsReturnDisabled(false);
-	};
-
+const Recipe: React.FC<RecipeProps> = ({ recipeText, isLoading }) => {
 	const handleShareClick = () => {
 		shareRecipe(recipeText);
 	};
@@ -45,7 +22,7 @@ const Recipe: React.FC<RecipeProps> = ({
 			<div className={styles.recipe__header}>
 				<h3 className={styles.recipe__title}>Recipe</h3>
 				<button
-					className={[styles.recipe__button, styles.btn__share].join(" ")}
+					className={styles.recipe__btn}
 					onClick={handleShareClick}
 					disabled={isLoading}
 				>
@@ -54,7 +31,7 @@ const Recipe: React.FC<RecipeProps> = ({
 				</button>
 			</div>
 			{recipeText ? (
-				<p className={styles.recipe__desc} style={{ whiteSpace: "pre-line" }}>
+				<p className={styles.recipe__desc}>
 					{isLoading ? "Generating recipe..." : recipeText}
 				</p>
 			) : (
@@ -63,24 +40,6 @@ const Recipe: React.FC<RecipeProps> = ({
 					like magic, your custom recipe will appear right here! 🍳🧁✨
 				</p>
 			)}
-			<div className={styles.recipe__buttons}>
-				<button
-					className={styles.recipe__button}
-					onClick={handleReturnClick}
-					disabled={isLoading || isReturnDisabled}
-				>
-					<FontAwesomeIcon icon={faArrowLeft} />
-					Return
-				</button>
-				<button
-					className={styles.recipe__button}
-					onClick={handleContinueClick}
-					disabled={isLoading || !isReturnDisabled}
-				>
-					<FontAwesomeIcon icon={faArrowRight} />
-					Continue
-				</button>
-			</div>
 		</div>
 	);
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,8 @@ interface IngredientOption {
 
 export default function Home() {
 	const [ingredients, setIngredients] = useState<IngredientOption[]>([]);
-	const [initialRecipe, setInitialRecipe] = useState<string>("");
-	const [continueRecipe, setContinueRecipe] = useState<string>("");
+	const [recipe, setRecipe] = useState<string>("");
 	const [isLoading, setIsLoading] = useState(false);
-	const [showInitialRecipe, setShowInitialRecipe] = useState(true);
 
 	const handleSearch = async () => {
 		setIsLoading(true);
@@ -30,27 +28,10 @@ export default function Home() {
 				.join(", ");
 			const response = await generateText(ingredientsList);
 			if (response?.generated_text) {
-				setInitialRecipe(response.generated_text);
+				setRecipe(response.generated_text);
 			}
 		}
 		setIsLoading(false);
-	};
-
-	const handleContinue = async () => {
-		if (initialRecipe) {
-			const ingredientsList = ingredients
-				.map((ingredient) => ingredient.value)
-				.join(", ");
-			const response = await generateText(ingredientsList, initialRecipe);
-			if (response?.generated_text) {
-				setContinueRecipe(response.generated_text);
-				setShowInitialRecipe(false);
-			}
-		}
-	};
-
-	const handleReturn = () => {
-		setShowInitialRecipe(true);
 	};
 
 	return (
@@ -63,12 +44,7 @@ export default function Home() {
 			/>
 			<section className={styles.middle}>
 				<AvatarAppIntro />
-				<Recipe
-					recipeText={showInitialRecipe ? initialRecipe : continueRecipe}
-					isLoading={isLoading}
-					onContinue={handleContinue}
-					onReturn={handleReturn}
-				/>
+				<Recipe recipeText={recipe} isLoading={isLoading} />
 			</section>
 		</main>
 	);
